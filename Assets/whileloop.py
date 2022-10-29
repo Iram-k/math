64ef5678d20508e1447e7a2f6b492c71a872b0ab
Your Name <you@example.com>
2022-10-29 13:49:34 +0530

    patterns

---
+++ Assets/whileloop.py
@@ -0,0 +1,15 @@
+# using while loop afte applying a condition
+
+a=1
+while a<10: # input condition
+    print (a)
+    if a==5:#break condition to terminate the code
+      break
+    a=a+1 # exit condition 
+
+b=1
+while b<10: # input condition
+    b=b+1
+    if b==5:
+     continue
+    print (b)
