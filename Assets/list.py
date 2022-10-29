d2db3e4a1ce72e023ed7db151d58754e1bba2d5b
Your Name <you@example.com>
2022-10-29 13:15:30 +0530

    rps game

--- Assets/list.py
+++ Assets/list.py
@@ -1,1 +1,18 @@
-a=[1,2,3,4]
+import random
+
+user=input("Enter a choice : (rock,paper,scissors)")
+pos=["rock","paper","scissors"]
+comp=random.choice(pos)
+print("computer's choice :",comp)
+
+# print user action and computer action
+if user==comp:
+    print("tie")
+elif user=="rock" and comp=="scissors":
+    print("user wins")
+elif user=="scissors" and comp=="paper":
+    print("user wins")
+elif user=="paper" and comp=="rock":
+    print("user wins")
+else:
+    print("computer wins")
